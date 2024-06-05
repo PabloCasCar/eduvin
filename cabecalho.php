ec252210bd67166bae96d4f686aa9af874d1715f
Pablo Carvalho <pabloduzinho@gmail.com>
2024-06-05 16:56:42 +0100

    Revert "eduvin_pap"
    This reverts commit f7eb3acd2e38bac98816e66cc39a970071e90ca8.

--- cabecalho.php
+++
@@ -1,9 +0,0 @@
-<header>
-        <a href="index.php"><img id="logo" src="images/EDUVIN.png" alt="Eduvin's logo"></a>
-        <ul class="menu">
-            <li><a href="login.php">Login</a></li>
-            <li><a href="#carrinho.php">Carrinho</a></li>
-            <li><a href="#roupas">Roupas</a></li>
-            <li><a href="#acessorios">Acessórios e Calçados</a></li>
-        </ul>  
-</header>
